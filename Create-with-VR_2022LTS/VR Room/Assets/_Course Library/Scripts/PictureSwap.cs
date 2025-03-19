0b4a1804e306958e1e71c0450d36dfba0dab787d
KamilaHr <K.HRYSHCHENKO-2022@hull.ac.uk>
2025-03-19 14:00:39 +0000

    FinishPictures

--- Create-with-VR_2022LTS/VR Room/Assets/_Course Library/Scripts/PictureSwap.cs
+++ Create-with-VR_2022LTS/VR Room/Assets/_Course Library/Scripts/PictureSwap.cs
@@ -5,38 +5,49 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class PictureSwap : MonoBehaviour
 {
-    public XRSocketInteractor socket;
+    private XRSocketInteractor socket;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         socket = GetComponent<XRSocketInteractor>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        // Ensure the entering object is a valid interactable
+        IXRSelectInteractable newObject = other.GetComponent<IXRSelectInteractable>();
+        if (newObject == null) return;
+
+        // Check if the socket already has an object
         if (socket.selectTarget != null)
         {
+            // Get the current object in the socket
             IXRSelectInteractable existingObject = socket.selectTarget;
             GameObject existingObjectGameObject = existingObject.transform.gameObject;
-
-            IXRSelectInteractable newObject = other.GetComponent<IXRSelectInteractable>();
             GameObject newObjectGameObject = newObject.transform.gameObject;
 
-            if (newObject != null)
-            {
-                // Store the previous position of the new object before swapping
-                Vector3 newObjectPreviousPosition = newObjectGameObject.transform.position;
+            // Store the previous position of the new object before swapping
+            Vector3 newObjectPreviousPosition = newObjectGameObject.transform.position;
 
-                // Remove the existing object from the socket
-                socket.interactionManager.SelectExit(socket, existingObject);
+            // Disable gravity and enable kinematic mode for smooth placement
+            Rigidbody existingRb = existingObjectGameObject.GetComponent<Rigidbody>();
+            Rigidbody newRb = newObjectGameObject.GetComponent<Rigidbody>();
 
-                // Move the old object to the previous position of the new object
-                existingObjectGameObject.transform.position = newObjectPreviousPosition;
+            existingRb.useGravity = false;
+            existingRb.isKinematic = true;
 
-                // Place the new object into the socket
-                socket.interactionManager.SelectEnter(socket, newObject);
-            }
+            newRb.useGravity = false;
+            newRb.isKinematic = true;
+
+            // Remove the existing object from the socket
+            socket.interactionManager.SelectExit(socket, existingObject);
+
+            // Move the old object to the previous position of the new object
+            existingObjectGameObject.transform.position = newObjectPreviousPosition;
+
+            // Place the new object into the socket
+            socket.interactionManager.SelectEnter(socket, newObject);
         }
     }
 }
